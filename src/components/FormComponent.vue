6a78246e4d5c43d12f1d0b934b5b741d45533112
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-01-31 01:08:57 +0530

    Refactoring going ahead

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -7,6 +7,7 @@
 </template>
   
 <script>
+import { validateField } from '../helpers/formValidation';
 import FormField from './FormField.vue';
 import FormSubmitButton from './FormSubmitButton.vue';
 
@@ -17,101 +18,26 @@ export default {
   },
   props: ['fields', 'attributeLabelColor', 'errors', 'submitButtonLabel', 'submitButtonColor'],
   methods: {
-    // validation methods
-    validateDateOfBirth(date) {
-      const selectedDate = new Date(date);
-      const currentDate = new Date();
-
-      return selectedDate > currentDate;
-    },
-
-    validatePANCardNumber(panNumber) {
-      return panNumber.length === 10;
-    },
-
-    validatePincode(pincode) {
-      const pincodeRegex = /^\d{6}$/;
-      return pincodeRegex.test(pincode);
-    },
-
-    validateFileSize(file) {
-      const maxFileSize = 5 * 1024 * 1024;
-      return file.size <= maxFileSize;
-    },
     handleFileUpload(event, fieldData) {
       const file = event.target.files[0];
       this.$emit('update-field', { fieldData, file });
     },
-
-
+    collectErrors() {
+      const allErrors = {};
+      this.fields.forEach(field => {
+        const fieldErrors = validateField(field);
+        if (fieldErrors.length) {
+          allErrors[field.code] = fieldErrors;
+        }
+      });
+      return allErrors;
+    },
     submit() {
-      let localErrors = { ...this.errors };
+      const collectedErrors = this.collectErrors();
 
-      localErrors = {};
+      this.$emit('update-errors', collectedErrors);
 
-      // Check for empty required fields
-      this.fields.forEach(field => {
-        if (field.is_required === 1 && !field.value) {
-          if (!localErrors[field.code]) {
-            localErrors[field.code] = [];
-          }
-          localErrors[field.code].push('Please fill this field.');
-        }
-      });
-
-      // Validate date_of_birth field
-      if (this.fields.some(field => field.code === "date_of_birth" && this.validateDateOfBirth(field.value))) {
-        if (!localErrors.date_of_birth) {
-          localErrors.date_of_birth = [];
-        }
-        localErrors.date_of_birth.push('Date of birth cannot be in the future.');
-      }
-
-      // Validate pan_card_number field
-      if (this.fields.some(field => field.code === "pan_card_number" && !this.validatePANCardNumber(field.value))) {
-        if (!localErrors.pan_card_number) {
-          localErrors.pan_card_number = [];
-        }
-        localErrors.pan_card_number.push('Invalid PAN (must be 10 characters).');
-      }
-
-      // Validate pincode field
-      if (this.fields.some(field => field.code === "pincode" && !this.validatePincode(field.value))) {
-        if (!localErrors.pincode) {
-          localErrors.pincode = [];
-        }
-        localErrors.pincode.push('Invalid pincode (must be 6 digits).');
-      }
-
-      // Validate passport issue date field
-      if (this.fields.some(field => field.code === "passport_issue_date" && this.validateDateOfBirth(field.value))) {
-        if (!localErrors.passport_issue_date) {
-          localErrors.passport_issue_date = [];
-        }
-        localErrors.passport_issue_date.push('Passport issue date cannot be in the future.');
-      }
-
-      // Validate passport expiry date field
-      if (this.fields.some(field => field.code === "passport_expiry_date" && !this.validateDateOfBirth(field.value))) {
-        if (!localErrors.passport_expiry_date) {
-          localErrors.passport_expiry_date = [];
-        }
-        localErrors.passport_expiry_date.push('Expired passport is not accepted.');
-      }
-
-      // Validate file input fields
-      this.fields.forEach(field => {
-        if (field.type === 'file' && field.value && !this.validateFileSize(field.value)) {
-          if (!localErrors[field.code]) {
-            localErrors[field.code] = [];
-          }
-          localErrors[field.code].push('File size must be less than or equal to 5 MB.');
-        }
-      });
-
-      this.$emit('update-errors', localErrors);
-
-      if (Object.keys(localErrors).length === 0) {
+      if (Object.keys(collectedErrors).length === 0) {
         // No errors, proceed with form submission
         const enteredValues = this.fields.map(field => `${field.name.en}: ${field.value}`);
         alert(`${this.jsonData.data.submit_success_content}\n Entered Values:\n${enteredValues.join('\n')}`);
--- src/components/FormSubmitButton.vue
+++ src/components/FormSubmitButton.vue
@@ -1,27 +1,27 @@
 <template>
-    <button type="submit" :style="{ backgroundColor: buttonColor }" class="form-submit">
-        {{ buttonText }}
-    </button>
+  <button type="submit" :style="{ backgroundColor: buttonColor }" class="form-submit">
+    {{ buttonText }}
+  </button>
 </template>
   
 <script>
 export default {
-    props: ['buttonText', 'buttonColor'],
+  props: ['buttonText', 'buttonColor'],
 };
 </script>
   
 <style scoped>
 .form-submit {
-    padding: 0.5rem 0.75rem 0.5rem 0.75rem;
-    border: none;
-    text-wrap: wrap;
-    font-weight: 700;
-    color: #fff;
-    cursor: pointer;
+  padding: 0.5rem 0.75rem 0.5rem 0.75rem;
+  border: none;
+  text-wrap: wrap;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
 }
 
 .form-submit:hover {
-    background-color: #2626c1 !important;
+  background-color: #2626c1 !important;
 }
 </style>
   
